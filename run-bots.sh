4e2f8ecffca34489561cc8c4d5bcf216e39c8823
beningapniprameswari <beningapniprameswari@gmail.com>
2025-06-01 22:53:42 +0700

    Delete run-bots.sh

--- run-bots.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python main.py --logic Greedy --email=befira@email.com --name=mangu --password=212425 --team etimo &
-python main.py --logic Greedy --email=befira1@email.com --name=raisya --password=212425 --team etimo &
-python main.py --logic Greedy --email=befira12@email.com --name=bening --password=212425 --team etimo &
-python main.py --logic Greedy --email=befira123@email.com --name=afifa --password=212425 --team etimo &
