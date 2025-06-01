67b23c20c4ca0a1eeca2450fba85d7ed216c6bde
beningapniprameswari <beningapniprameswari@gmail.com>
2025-06-01 22:53:54 +0700

    Delete run-bots.bat

--- run-bots.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start cmd /c "python main.py --logic Greedy --email=befira@email.com --name=mangu --password=212425 --team etimo"
-start cmd /c "python main.py --logic Greedy --email=befira1@email.com --name=raisya --password=212425 --team etimo"
-start cmd /c "python main.py --logic Greedy --email=befira12@email.com --name=bening --password=212425 --team etimo"
-start cmd /c "python main.py --logic Greedy --email=befira123@email.com --name=afifa --password=212425 --team etimo"
